82614ecc91a6d1653af7157627e596b23ae96dda
Stephanie Friend <stephaniefriend91@gmail.com>
2020-06-08 10:01:56 -0600

    got serializer to talk to poro

--- app/poro/travel.rb
+++ app/poro/travel.rb
@@ -1,6 +1,13 @@
 class Travel
+  attr_reader :id
 
   def initialize(start, ending)
+    @id = "#{start}" + "#{ending}"
+  end
 
+  def travel_info
+    {
+        :travel_time =>  "21 min "
+    }
   end
 end
