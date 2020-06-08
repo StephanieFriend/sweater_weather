29bdf2d7d478616f2f84a3b32dc42a8199837183
Stephanie Friend <stephaniefriend91@gmail.com>
2020-06-08 10:33:24 -0600

    established connection with zomato

---
+++ app/services/zomato_services.rb
@@ -0,0 +1,13 @@
+class ZomatoServices
+
+  def self.get_json(lat, lon, type)
+    response = Faraday.get('https://developers.zomato.com/api/v2.1/search') do |req|
+      req.header[:user_key] = ENV['ZOMATO_API_KEY']
+      req.params[:count] = 1
+      req.params[:lat] = lat
+      req.params[:lon] = lon
+      req.params[:category]
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
