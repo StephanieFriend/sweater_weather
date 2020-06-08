b72a566f12b13821ad5db08a524459f32e83db7d
Stephanie Friend <stephaniefriend91@gmail.com>
2020-06-08 10:20:35 -0600

    returned distance

--- app/controllers/api/v1/travels_controller.rb
+++ app/controllers/api/v1/travels_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::TravelsController < ApplicationController
 
   def show
-    binding.pry
     render :json => TravelSerializer.new(Travel.new(params[:start], params[:ending]))
   end
 end
--- app/poro/travel.rb
+++ app/poro/travel.rb
@@ -3,11 +3,15 @@ class Travel
 
   def initialize(start, ending)
     @id = "#{start}" + "#{ending}"
+    @start = start
+    @ending = ending
+    @direction = DirectionServices.get_json(@start, @ending)
+    binding.pry
   end
 
   def travel_info
     {
-        :travel_time =>  "21 min "
+        :travel_time =>  Distance.new(@direction)
     }
   end
 end
