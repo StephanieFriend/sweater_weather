fbcc621a9698a3ee58a2606b383922b5fb35d152
Stephanie Friend <stephaniefriend91@gmail.com>
2020-06-09 16:55:13 -0600

    Wrote sad path test for login

--- spec/requests/api/v1/road_trip/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip/road_trip_request_spec.rb
@@ -52,4 +52,38 @@ RSpec.describe 'Road Trip API' do
     expect(json[:data][:attributes]).to have_key :destination
     expect(json[:data][:attributes][:destination]).to eq("Pueblo,CO")
   end
+
+  it 'Can receive improper credentials and return error message' do
+    post '/api/v1/users', params: {
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password"
+    }
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    post '/api/v1/sessions', params: {
+        email: "whatever@example.com",
+        password: "password"
+    }
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    login_json = JSON.parse(response.body, symbolize_names: true)
+    key = login_json[:data][:attributes][:api_key]
+
+    post '/api/v1/road_trip', params: {
+        origin: "Denver,CO",
+        destination: "Pueblo,CO",
+        api_key: key + "1"
+    }
+
+    expect(response.status).to eq(401)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data][:attributes]).to have_key :error_message
+    expect(json[:data][:attributes][:error_message]).to eq("Must use a valid API key for request")
+  end
 end
