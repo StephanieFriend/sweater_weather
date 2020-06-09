972d8bdb38c95101c1afe2b893a06feaed197d24
Stephanie Friend <stephaniefriend91@gmail.com>
2020-06-09 13:47:36 -0600

    Update with new serializer format

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,11 +2,13 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-
     if user && user.authenticate(params[:password])
-      render :json => UserSerializer.new(user), status: :ok
+      render :json => UserResponseSerializer.new(UserResponse.new(user.id, user.email, user.api_key, nil)),
+             status: :ok
     else
-      render :json => User.login_error, status: :bad_request
+      render :json => UserResponseSerializer.new(UserResponse.new(nil, nil, nil,
+                                                                  "Username or Password is incorrect")),
+             status: :bad_request
     end
   end
 
