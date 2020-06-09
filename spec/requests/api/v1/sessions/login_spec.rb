c36da33621a6f50e7b4cc04531ae0606c267f18d
Stephanie Friend <stephaniefriend91@gmail.com>
2020-06-09 00:44:08 -0600

    Write test for login

--- spec/requests/api/v1/sessions/login_spec.rb
+++ spec/requests/api/v1/sessions/login_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe 'Session Login' do
   end
 
   it 'Can input proper credentials and return login info' do
+    post '/api/v1/users', params: {
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password"
+    }
+
+    expect(response).to be_successful
+
     post '/api/v1/sessions', params: {
         email: "whatever@example.com",
         password: "password"
@@ -23,7 +31,32 @@ RSpec.describe 'Session Login' do
     expect(response).to be_successful
 
     json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data][:attributes]
 
-    binding.pry
+    expect(json.class).to eq(Hash)
+    expect(data).to have_key :email
+    expect(data[:email]).to eq("whatever@example.com")
+    expect(data).to have_key :api_key
+    expect(data[:api_key].class).to eq(String)
+  end
+
+  it 'Can input improper credentials and return error' do
+    post '/api/v1/users', params: {
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password"
+    }
+
+    expect(response).to be_successful
+
+    post '/api/v1/sessions', params: {
+        email: "whatever@example.com",
+        password: "pass"
+    }
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(400)
+
   end
 end
