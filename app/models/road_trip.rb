338a94515bcc278958ef620448788be957529f72
Stephanie Friend <stephaniefriend91@gmail.com>
2020-06-09 19:29:28 -0600

    Cleaned up un used method

--- app/models/road_trip.rb
+++ app/models/road_trip.rb
@@ -5,9 +5,4 @@ class RoadTrip < ApplicationRecord
   validates :destination, presence: true
   validates :api_key, presence: true
 
-  def self.api_error
-    {
-        :error_message => "Must use a valid API key for request"
-    }
-  end
 end
